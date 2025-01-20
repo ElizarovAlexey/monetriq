b51e38343115df638b79a39acad4ec0c7f15a0e1
Alexei Elizarov <alexei@bend.md>
2025-01-20 11:59:25 +0200

    docker & nginx & deploy settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
 // @ts-check
-const eslint = require('@eslint/js');
-const tseslint = require('typescript-eslint');
-const angular = require('angular-eslint');
-const unusedImports = require('eslint-plugin-unused-imports');
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const unusedImports = require("eslint-plugin-unused-imports");
 
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
     plugins: {
-      'unused-imports': unusedImports,
+      "unused-imports": unusedImports,
     },
     extends: [
       eslint.configs.recommended,
@@ -37,15 +37,15 @@ module.exports = tseslint.config(
           style: "kebab-case",
         },
       ],
-      'no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
         {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
         },
       ],
     },
